d34ddd611dacbff8027fe052cf1a64491e60af61
Mohamed Goui <goui.mohamed01@gmail.com>
2020-09-10 18:25:15 +0100

    ddl

---
+++ ddl-check.sql
@@ -0,0 +1,24 @@
+CREATE TABLE Product(
+  Product_id VARCHAR2(20) PRIMARY KEY,
+  Product_Name VARCHAR2(20) NOT NULL,
+  Price NUMBER CHECK ( Price >  0 )
+  );
+  
+  CREATE TABLE Customer(
+  Customer_id VARCHAR2(20) PRIMARY KEY,
+  Customer_Name VARCHAR2(20) NOT NULL,
+  Customer_tel NUMBER
+  );
+  
+  CREATE TABLE Orders(
+    FOREIGN KEY (Customer_id) REFERENCES Customer(Customer_id),
+    FOREIGN KEY (Product_id) REFERENCES Product(Product_id),
+    Quantity NUMBER,
+    Total_amount NUMBER,
+    CONSTRAINT pk_primary PRIMARY KEY(Customer_id, Product_id)
+  );
+  
+  ALTER TABLE Product ADD Category VARCHAR2(20);
+  ALTER TABLE Orders ADD OrderDate DATE DEFAULT SYSDATE;
+  
+  
